97b69c39cca31c96777a4d1bb03723c54ba65354
Zhaidar <149644540+Zhaidar-gths@users.noreply.github.com>
2023-11-16 10:31:27 +0600

    dictonaries

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+# task 1
+
+Dog={}
+
+print(Dog)
+
+
+# task 2
+
+Dog={}
+Dog['Name']= "Golden Retriever"
+Dog['Height']='22-24 inches (56-61cm)'
+Dog['Weight']='60-80 pounds (27-36 kg)'
+Dog['Colors']='Golden'
+print(Dog)
